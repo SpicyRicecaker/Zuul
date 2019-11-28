f1e95fa1aea2f916c4eb976cd7d9dd81e4ebac28
Andy Li <awesomenessisgolden@gmail.com>
2019-11-27 20:35:34 -0800

    Done with init, now doing commands

--- Game.cpp
+++ Game.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-void init(bool& running, vector<Command*>* commandsptr);
+void init(bool& running, vector<Command*>* commandsptr, Room* currentRoom);
 
 int main(){
 
@@ -27,13 +27,14 @@ int main(){
   vector <Command*> commands;
   vector <Command*>* commandsptr = &commands;
   
-  //Plan for commands: Have vector of Cstrings.  
+  //Current Room
+  Room* currentRoom;
 
   cout << "Welcome to the world of Zuul! I'd like to thank my friend Peter Jin for making this game possible, and for helping me code. Peter Pan's Land is a world set in a dystopian future where capitalism has led to the rise of Peter Jin & Peter Pan. Anyways, have fun playing!" << endl;
   
   bool running;
  
-  init(running, commandsptr);
+  init(running, commandsptr, currentRoom);
 
   while(running){
     vector<Command*>::iterator comIt;
@@ -41,9 +42,10 @@ int main(){
   }
 }
 
-void init(bool& running, vector<Command*>* commandsptr){
+void init(bool& running, vector<Command*>* commandsptr, Room* currentRoom){
   running = true;  
-  commandsptr->push_back(new Command(new char[4]{'H','e','a','d'}));
-  commandsptr->push_back(new Command(new char[4]{'G','r','a','b'}));
-  commandsptr->push_back(new Command(new char[4]{'T','o','s','s'}));
+  commandsptr->push_back(new Command((char*)"Head"));
+  commandsptr->push_back(new Command((char*)"Toss"));
+  commandsptr->push_back(new Command((char*)"Grab"));
+  currentRoom = new Room((char*)"Peter Pan's Jam", new vector<Item*>, new map<char*, Room*>);
 }
